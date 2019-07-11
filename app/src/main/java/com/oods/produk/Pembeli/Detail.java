639cc5f2ca1d7ee4045035a666c260fe96554c87
sehatmaru <sehatmaru@gmail.com>
2019-07-11 13:40:32 +0700

    Add detail activity

--- app/src/main/java/com/oods/produk/Pembeli/Detail.java
+++ app/src/main/java/com/oods/produk/Pembeli/Detail.java
@@ -2,18 +2,15 @@ package com.oods.produk.Pembeli;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.oods.produk.R;
 
-public class Home extends AppCompatActivity {
+public class Detail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_detail);
     }
 }
--- app/src/main/java/com/oods/produk/Pembeli/HomeFragment.java
+++ app/src/main/java/com/oods/produk/Pembeli/HomeFragment.java
@@ -1,10 +1,12 @@
 package com.oods.produk.Pembeli;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
@@ -22,9 +24,32 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button btnDetail = view.findViewById(R.id.btnDetail);
+        Button btnDetail1 = view.findViewById(R.id.btnDetail1);
+
+        btnDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDetail();
+            }
+        });
+
+        btnDetail1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDetail();
+            }
+        });
+
+        return view;
+    }
+
+    private void openDetail(){
+        Intent intent = new Intent(this.getContext(), Detail.class);
+        startActivity(intent);
     }
 
 }
