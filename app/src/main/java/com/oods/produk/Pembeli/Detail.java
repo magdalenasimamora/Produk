2ed47fa87b16f6c49e331df317a316e3287a0fbf
sehatmaru <sehatmaru@gmail.com>
2019-07-15 11:44:55 +0700

    Add Inbox

--- app/src/main/java/com/oods/produk/Pembeli/Detail.java
+++ app/src/main/java/com/oods/produk/Pembeli/Detail.java
@@ -2,7 +2,10 @@ package com.oods.produk.Pembeli;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.oods.produk.R;
 
@@ -12,5 +15,14 @@ public class Detail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Button btnPesan = findViewById(R.id.btnPesan);
+        btnPesan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Inbox.class);
+                startActivity(intent);
+            }
+        });
     }
 }
